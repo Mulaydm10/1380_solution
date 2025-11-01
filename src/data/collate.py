28fa30f5677ee6c5c7f0e6f916a3f11bdaa3b64a
Mulaydm10 <dhruvmulay10@gmail.com>
2025-11-01 07:52:45 +0530

    g

--- src/data/collate.py
+++ src/data/collate.py
@@ -17,15 +17,58 @@ def batch_to_gpu(batch):
 
 import torch
 import torch
-from torch.utils.data.dataloader import default_collate
+import numpy as np
+
+def pad_collate(batch):
+    """
+    A collate function that pads variable-length tensors to the max length in a batch.
+    It also creates an attention mask for the padded elements.
+    """
+    # Separate keys that need padding from those that don't
+    variable_len_keys = ["bboxes", "classes", "masks"]
+    result = {}
+
+    # Handle standard collation for most keys
+    for key in batch[0].keys():
+        if key != "bboxes_3d_data":
+            result[key] = torch.utils.data.default_collate([d[key] for d in batch])
+
+    # --- Custom Padding for bboxes_3d_data ---
+    bbox_data = [d["bboxes_3d_data"] for d in batch]
+    result["bboxes_3d_data"] = {}
+
+    # Find the max number of objects in this batch
+    max_num_objects = max(d["bboxes"].shape[2] for d in bbox_data)
+
+    for key in variable_len_keys:
+        padded_tensors = []
+        for i in range(len(batch)):
+            tensor = bbox_data[i][key]
+            num_objects = tensor.shape[2]
+            pad_size = max_num_objects - num_objects
+            
+            # The shape is (1, 1, num_objects, ...), so padding is on the 3rd dimension (index 2)
+            # We need to define padding for all dimensions after the one we are padding
+            # (pad_left, pad_right, pad_top, pad_bottom, ...)
+            padding = (0, 0) * (tensor.ndim - 3) + (0, pad_size)
+            padded_tensor = np.pad(tensor, ((0,0), (0,0), (0, pad_size)) + ((0,0),)*(tensor.ndim-3), 'constant', constant_values=0)
+            padded_tensors.append(padded_tensor)
+        
+        result["bboxes_3d_data"][key] = torch.from_numpy(np.concatenate(padded_tensors, axis=0))
+
+    # Create an attention mask for the bounding boxes
+    # Shape: (B, 1, 1, max_num_objects)
+    attention_masks = []
+    for i in range(len(batch)):
+        num_objects = bbox_data[i]["bboxes"].shape[2]
+        mask = np.zeros((1, 1, max_num_objects), dtype=np.float32)
+        mask[:, :, :num_objects] = 1.0
+        attention_masks.append(mask)
+    
+    result["bboxes_3d_data"]["attention_mask"] = torch.from_numpy(np.concatenate(attention_masks, axis=0))
+
+    return result
 
 class Collate:
     def __call__(self, batch):
-        # First, use the default collate to handle most data types
-        collated_batch = default_collate(batch)
-
-        # The default collate will turn our list of stacked tensors into a single tensor,
-        # which is what we want. No special handling is needed if __getitem__ returns stacked tensors.
-        # This function remains for clarity and future modifications.
-
-        return collated_batch
+        return pad_collate(batch)
