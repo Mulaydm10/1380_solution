bb56ac34afc90275a899792696d3969dfc6e9a43
Mulaydm10 <dhruvmulay10@gmail.com>
2025-11-03 23:13:33 +0530

    ss

--- inference.py
+++ inference.py
@@ -153,15 +153,14 @@ if __name__ == "__main__":
 
             # Generate conditioning embedding
             with torch.no_grad():
-                                        bboxes_list = [
-                                            {
-                                                'bboxes': batch_data['bboxes_3d_data']['bboxes'][i],
-                                                'classes': batch_data['bboxes_3d_data']['classes'][i],
-                                                'masks': batch_data['bboxes_3d_data']['masks'][i],
-                                            }
-                                            for i in range(len(batch_data['ride_id']))
-                                        ]
-                                        if 'bev_grid' not in batch_data:
+                                                    bboxes_list = [
+                                                        {
+                                                            'bboxes': batch_data['bboxes_3d_data']['bboxes']['data'][i],
+                                                            'classes': batch_data['bboxes_3d_data']['classes']['data'][i],
+                                                            'masks': batch_data['bboxes_3d_data']['masks']['data'][i],
+                                                        }
+                                                        for i in range(len(batch_data['ride_id']))
+                                                    ]                                        if 'bev_grid' not in batch_data:
                                             raise ValueError("BEV missing – Check dataset.py raster call")
                                         cond_emb = embedder(
                                             bboxes_list,
