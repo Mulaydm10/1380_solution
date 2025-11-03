b9f89ebafe59d2f66c21ef5597babda87154baf5
Mulaydm10 <dhruvmulay10@gmail.com>
2025-11-04 02:03:28 +0530

    asa

--- inference.py
+++ inference.py
@@ -100,6 +100,7 @@ if __name__ == "__main__":
 
     # Create a config object and then instantiate the model
     model_config_dict['input_size'] = (16, 80, 32, 32)
+    model_config_dict['patch_size'] = (1, 2, 2)
     config = STDiT3Config(**model_config_dict)
     model = STDiT3(config).to(device, dtype)
     # Dynamically find the latest checkpoint from training_checkpoints
